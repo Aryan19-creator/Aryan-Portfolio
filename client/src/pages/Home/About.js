3811dff80b5b7091095bf392f54f068e4ffc4eda
Aryan_19 <an2013.ak@gmail.com>
2024-06-07 02:27:34 +0530

    about section

---
+++ client/src/pages/Home/About.js
@@ -0,0 +1,50 @@
+import SectionTitle from "../../components/SectionTitle";
+
+function About(){
+    return (
+      <div>
+        <SectionTitle title="About" />
+        <div className="flex">
+          <div className="h-[70vh] w-1/2">
+            <dotlottie-player
+              src="https://lottie.host/d87d83ff-1a0e-47e8-8686-0846dba3c956/DXlswqrvUn.json"
+              background="transparent"
+              speed="1"
+              autoplay
+            ></dotlottie-player>
+            </div>
+            <div className="flex flex-col gap-5 w-1/2">
+              <p className="text-white">
+                I am a passionate and dedicated software engineer with a robust
+                academic background in Information Technology from the Birla
+                Institute of Technology, Mesra. My academic journey, combined
+                with practical experience gained through internships at The
+                Software Practice PTE LTD. and CRIO.DO, has equipped me with a
+                diverse skill set in software development. I have proficiency in
+                multiple programming languages, including Java, C++, C#, and
+                JavaScript, and hands-on experience with frameworks like
+                React.js and Node.js. My projects, such as "Shopyay!" and
+                "Netflix Clone," showcase my ability to create dynamic and
+                responsive web applications, demonstrating my strong
+                understanding of both frontend and backend development.
+                Participation in coding competitions and solving complex
+                problems on platforms like Codeforces and LeetCode has further
+                honed my analytical and problem-solving skills.
+              </p>
+              <p className="text-white">
+                Looking ahead, my long-term professional goal is to evolve into
+                a lead software engineer, where I can drive innovative projects
+                and mentor emerging talent. I am committed to continuous
+                learning and staying at the forefront of technological
+                advancements, aiming to contribute to solutions that enhance
+                user experiences and drive business success. My enthusiasm for
+                problem-solving and passion for technology drive me to seek
+                opportunities where I can make a meaningful impact.
+              </p>
+            </div>
+          </div>
+        </div>
+      
+    );
+}
+export default About;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header";
 import Intro from "./Intro";
+import About from "./About";
 
 function Home()
 {
@@ -9,7 +10,9 @@ function Home()
             <Header/>
             <div className="bg-primary px-40">
                 <Intro/>
+                <About/>
             </div>
+          
             
         </div>
     )
